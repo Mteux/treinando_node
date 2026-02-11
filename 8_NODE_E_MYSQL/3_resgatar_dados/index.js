638027db5e36ba8801d39094e364de2e97b908ff
Mateus Ferreira da Silva <mateus.ferreira@CREDCESTA.local>
2026-02-11 11:00:52 -0300

    Resgatando dados do sql

---
+++ 8_NODE_E_MYSQL/3_resgatar_dados/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const db = require('./db')
+
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES (?, ?)`
+    db.run(sql, [title, pageqty], function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.get('/books', (req, res) => {
+    const sql = "SELECT * FROM books"
+
+    db.all(sql, function(err, data) {
+
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        const books = data
+        console.log(books);
+
+        res.render('books', {books})
+        
+    })
+})
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+        
+})
+    
